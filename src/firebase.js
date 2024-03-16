a396132f84ec90c691ca90a4bb7948e963803cf6
luisr0514 <68670791+luisr0514@users.noreply.github.com>
2024-03-15 21:56:22 -0400

    inicio
    creación de documentos react y instalación del firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3wuhBrlPEWaW_fg6vCONjNRkGs_Xp4fY",
+  authDomain: "agrupana.firebaseapp.com",
+  projectId: "agrupana",
+  storageBucket: "agrupana.appspot.com",
+  messagingSenderId: "478717383297",
+  appId: "1:478717383297:web:40770d48ab9dd2311df380"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
