13ec89c3e47078899b9abe2d8db8d2ad5a537749
paulabricenob <pbriceno@correo.unimet.edu.ve>
2024-03-23 13:18:28 -0400

    intento de decoracion en el navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,27 +8,40 @@ const Navbar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg " >
-        <div className={styles.navbar}>
+        <div className={styles.headline}>
+          
           <a className="navbar-brand" href="#">
-            <img src={Logo}  width="100" height="50" className="d-inline-block align-text-top"/>
+            <img src={Logo}  className={styles.logoAgrupana1}/>
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav mx auto">
-              <li className="nav-item">
-                <Link className="nav-link " to='/'>Inicio</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to='/agrupaciones'>Agrupaciones Estudiantiles</Link>
-              </li>
+
+              <div className= {styles.inicioWrapper}>
+                <li className={styles.inicio}>
+                  <Link className="nav-link " to='/'>Inicio</Link>
+                </li>
+              </div>
+
+              <div className= {styles.agrupacionesWrapper}>
+                <li className={styles.agrupaciones}>
+                  <Link className="nav-link" to='/agrupaciones'>Agrupaciones Estudiantiles</Link>
+                </li>
+              </div>
+
               <li className="nav-item">
                 <Link className="nav-link" to='/inicio_sesion'>Iniciar Sesión</Link>
               </li>
-              <li className="nav-item">
+              
+              <div className = {styles.registrateWrapper}>
+                <li className={styles.registrate}>
                 <Link className="nav-link" to='/registrarse'>Regístrate</Link>
-              </li>
+                </li>
+              </div>
+              
+          
              {/*
              <li className="nav-item">
                 <button type="button" onClick={() => logOut()}>cerrar sesion</button>
