59d0c866f0b7a64ad79216671d8a5cfe3d4c1790
paulabricenob <pbriceno@correo.unimet.edu.ve>
2024-03-31 17:15:31 -0400

    imagenes en las agrupaciones

--- src/pages/AgrupacionPage.jsx
+++ src/pages/AgrupacionPage.jsx
@@ -20,8 +20,10 @@ const AgrupacionPage = () => {
         <Navbar/>
         <div>
           <h1>{agrupacion.nombre}</h1>
+          <img src={agrupacion.logo}/>
           <h2>Quienes Somos?</h2>
           <p>{agrupacion.somos}</p>
+          <img src={agrupacion.imagen}/>
           <h2>Por qué pertenecer a {agrupacion.nombre}?</h2>
           <p>{agrupacion.pertenecer}</p>
           <p>Misión: {agrupacion.mision}</p>
