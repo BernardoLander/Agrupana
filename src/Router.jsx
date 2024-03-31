80b26ab1ea1f7566835784cb32e02c6e59e8d621
paulabricenob <pbriceno@correo.unimet.edu.ve>
2024-03-31 01:14:40 -0400

    changes

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,6 +4,7 @@ import Registro from './pages/Registro'
 import GroupPage from './pages/GroupPage';
 import PerfilUsuario from './pages/PerfilUsuario';
 import HomePage from './pages/HomePage';
+import AdditionalInfo from './pages/additional-info';
 
 export const router = createBrowserRouter([
 
@@ -15,16 +16,17 @@ export const router = createBrowserRouter([
         path: '/registro',
         element: <Registro/>
     },
-
+    {
+        path:'/additional-info',
+        element:<AdditionalInfo/>
+    },
     {
         path:'/',
         element:<HomePage/>
-
     },
     {
         path:'/perfil',
         element:<PerfilUsuario/>
-
     },
     {
         path:'/agrupaciones',
